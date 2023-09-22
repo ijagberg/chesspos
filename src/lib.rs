9070a468230ab25aab77b8c1671f5495846ce354
ijagberg <ijagberg@gmail.com>
2023-09-22 12:14:32 +0200

    Add impl From<(File,Rank)> for Position

--- src/lib.rs
+++ src/lib.rs
@@ -217,6 +217,12 @@ impl Position {
     }
 }
 
+impl From<(File, Rank)> for Position {
+    fn from((file, rank): (File, Rank)) -> Self {
+        Self::new(file, rank)
+    }
+}
+
 impl Debug for Position {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         Display::fmt(&self, f)
